86dc198c072e27fe1a191a53f893eb588dc60405
biobios <biobios1024@gmail.com>
2024-07-22 01:29:58 +0900

    refactor: Clean up code

--- main.cpp
+++ main.cpp
@@ -4,25 +4,21 @@
 #include <array>
 #include <cmath>
 
-float blue[] = { 0.0, 0.0, 1.0, 1.0 };
-float white[] = { 1.0, 1.0, 1.0, 1.0 };
-float white_and_transparent[] = { 1.0, 1.0, 1.0, 0.5 };
-float black[] = { 0.0, 0.0, 0.0, 1.0 };
-float transparent[] = { 0.0, 0.0, 0.0, 0.0 };
+constexpr float blue[] = { 0.0, 0.0, 1.0, 1.0 };
+constexpr float white[] = { 1.0, 1.0, 1.0, 1.0 };
+constexpr float white_and_transparent[] = { 1.0, 1.0, 1.0, 0.5 };
+constexpr float black[] = { 0.0, 0.0, 0.0, 1.0 };
+constexpr float transparent[] = { 0.0, 0.0, 0.0, 0.0 };
 
 class MassPoint
 {
 public:
-    double mass;
-    double position[3];
+    double height;
     double velocity;
 public:
-    MassPoint(double m, double x, double y, double z, double v = 0)
+    MassPoint(double y = 0, double v = 0)
     {
-		mass = m;
-		position[0] = x;
-		position[1] = y;
-		position[2] = z;
+		height = y;
 		velocity = v;
     }
 
@@ -34,10 +30,50 @@ public:
     void update(double dv, double dt)
     {
         velocity += dv - velocity * 0.001;
-        velocity -= position[1] * 0.0001;
+        velocity -= height * 0.0001;
 //        velocity += dv;
-        position[1] += velocity * dt;
+        height += velocity * dt;
     }
+};
+
+struct Params {
+    float p[4];
+    Params() = default;
+    Params(float const* p) {
+        for (int i = 0; i < 4; i++)
+        {
+            this->p[i] = p[i];
+        }
+    }
+
+    Params& maltiply(float a) {
+        for (int i = 0; i < 3; i++)
+        {
+			p[i] *= a;
+		}
+		return *this;
+	}
+
+    Params& alpha(float a) {
+        p[3] = a;
+        return *this;
+    }
+    operator float* () { return p; }
+};
+
+struct Material {
+	Params ambient;
+	Params diffuse;
+	Params specular;
+	float shininess;
+	Material() = default;
+	Material(Params const& a, Params const& d, Params const& s, float sh) : ambient(a), diffuse(d), specular(s), shininess(sh) {}
+    void apply() {
+		glMaterialfv(GL_FRONT, GL_AMBIENT, ambient);
+		glMaterialfv(GL_FRONT, GL_DIFFUSE, diffuse);
+		glMaterialfv(GL_FRONT, GL_SPECULAR, specular);
+		glMaterialf(GL_FRONT, GL_SHININESS, shininess);
+	}
 };
 
 class WaterSurface
@@ -45,6 +81,8 @@ class WaterSurface
     std::vector<std::vector<MassPoint*>> points;
     double tension;
 public:
+    Material material = {};
+
     WaterSurface(uint64_t n, uint64_t m, double t)
     {
 		tension = t;
@@ -54,7 +92,7 @@ public:
 			points[i].resize(m);
             for (uint64_t j = 0; j < m; j++)
             {
-				points[i][j] = new MassPoint(1.0, i * 0.1, 0, j * 0.1);
+				points[i][j] = new MassPoint();
 			}
 		}
 	}
@@ -66,7 +104,7 @@ public:
 
     void setHeight(uint64_t i, uint64_t j, double h)
     {
-        points[i][j]->position[1] = h;
+        points[i][j]->height = h;
     }
 
     void update(double dt)
@@ -75,21 +113,21 @@ public:
 
         for (size_t i = 0; i < points.size() - 1; i++) {
             for (size_t j = 0; j < points[0].size(); j++) {
-                force[i][j] += points[i + 1][j]->position[1];
-                force[i+1][j] += points[i][j]->position[1];
+                force[i][j] += points[i + 1][j]->height;
+                force[i+1][j] += points[i][j]->height;
 
-                force[i][j] -= points[i][j]->position[1];
-                force[i+1][j] -= points[i+1][j]->position[1];
+                force[i][j] -= points[i][j]->height;
+                force[i+1][j] -= points[i+1][j]->height;
             }
         }
 
         for (size_t i = 0; i < points.size(); i++) {
             for (size_t j = 0; j < points[0].size() - 1; j++) {
-                force[i][j] += points[i][j + 1]->position[1];
-                force[i][j + 1] += points[i][j]->position[1];
+                force[i][j] += points[i][j + 1]->height;
+                force[i][j + 1] += points[i][j]->height;
 
-                force[i][j] -= points[i][j]->position[1];
-                force[i][j + 1] -= points[i][j + 1]->position[1];
+                force[i][j] -= points[i][j]->height;
+                force[i][j + 1] -= points[i][j + 1]->height;
             }
         }
 
@@ -100,116 +138,85 @@ public:
         }
     }
 
+    static constexpr double NORMAL_Y = 1;
+
     void draw()
     {
 
-        std::vector<std::vector<std::array<double, 3>>> normals(points.size(), std::vector<std::array<double, 3>>(points[0].size(), {0, 0, 0}));
+        std::vector<std::vector<std::array<double, 2>>> normals(points.size(), std::vector<std::array<double, 2>>(points[0].size(), {0, 0}));
         for (size_t i = 1; i < points.size() - 1; i++) {
             for (size_t j = 1; j < points[0].size() - 1; j++) {
-                normals[i][j][0] = points[i - 1][j]->position[1] - points[i + 1][j]->position[1];
-                normals[i][j][1] = 2;
-                normals[i][j][2] = points[i][j - 1]->position[1] - points[i][j + 1]->position[1];
+                normals[i][j][0] = points[i - 1][j]->height - points[i + 1][j]->height;
+                normals[i][j][1] = points[i][j - 1]->height - points[i][j + 1]->height;
             }
         }
 
-        for (size_t i = 1; i < points.size() - 1; i++) {
-            normals[i][0][0] = points[i - 1][0]->position[1] - points[i + 1][0]->position[1];
-            normals[i][0][1] = 2;
-            normals[i][0][2] = (points[i][0]->position[1] - points[i][1]->position[1]) * 2;
+        size_t last_x = points.size() - 1;
+        size_t last_y = points[0].size() - 1;
 
-            size_t last = points[0].size() - 1;
-            normals[i][last][0] = points[i - 1][last]->position[1] - points[i + 1][last]->position[1];
-            normals[i][last][1] = 2;
-            normals[i][last][2] = (points[i][last - 1]->position[1] - points[i][last]->position[1]) * 2;
+        for (size_t i = 1; i < points.size() - 1; i++) {
+            normals[i][0][0] = points[i - 1][0]->height - points[i + 1][0]->height;
+            normals[i][0][1] = (points[i][0]->height - points[i][1]->height) * 2;
+
+            normals[i][last_y][0] = points[i - 1][last_y]->height - points[i + 1][last_y]->height;
+            normals[i][last_y][1] = (points[i][last_y - 1]->height - points[i][last_y]->height) * 2;
         }
 
         for (size_t i = 1; i < points[0].size() - 1; i++) {
-			normals[0][i][0] = (points[0][i]->position[1] - points[1][i]->position[1]) * 2;
-			normals[0][i][1] = 2;
-			normals[0][i][2] = points[0][i - 1]->position[1] - points[0][i + 1]->position[1];
+			normals[0][i][0] = (points[0][i]->height - points[1][i]->height) * 2;
+			normals[0][i][1] = points[0][i - 1]->height - points[0][i + 1]->height;
 
-			size_t last = points.size() - 1;
-			normals[last][i][0] = (points[last][i]->position[1] - points[last - 1][i]->position[1]) * 2;
-			normals[last][i][1] = 2;
-			normals[last][i][2] = points[last][i - 1]->position[1] - points[last][i + 1]->position[1];
+			normals[last_x][i][0] = (points[last_x - 1][i]->height - points[last_x][i]->height) * 2;
+			normals[last_x][i][1] = points[last_x][i - 1]->height - points[last_x][i + 1]->height;
         }
 
-        normals[0][0][0] = (points[0][0]->position[1] - points[1][0]->position[1]) * 2;
-        normals[0][0][1] = 2;
-        normals[0][0][2] = (points[0][0]->position[1] - points[0][1]->position[1]) * 2;
+        normals[0][0][0] = (points[0][0]->height - points[1][0]->height) * 2;
+        normals[0][0][1] = (points[0][0]->height - points[0][1]->height) * 2;
 
-        normals[0][points[0].size() - 1][0] = (points[0][points[0].size() - 1]->position[1] - points[1][points[0].size() - 1]->position[1]) * 2;
-        normals[0][points[0].size() - 1][1] = 2;
-        normals[0][points[0].size() - 1][2] = (points[0][points[0].size() - 2]->position[1] - points[0][points[0].size() - 1]->position[1]) * 2;
+        normals[0][last_y][0] = (points[0][last_y]->height - points[1][last_y]->height) * 2;
+        normals[0][last_y][1] = (points[0][last_y - 1]->height - points[0][last_y]->height) * 2;
 
-        normals[points.size() - 1][0][0] = (points[points.size() - 1][0]->position[1] - points[points.size() - 2][0]->position[1]) * 2;
-        normals[points.size() - 1][0][1] = 2;
-        normals[points.size() - 1][0][2] = (points[points.size() - 1][1]->position[1] - points[points.size() - 1][0]->position[1]) * 2;
+        normals[last_x][0][0] = (points[last_x - 1][0]->height - points[last_x][0]->height) * 2;
+        normals[last_x][0][1] = (points[last_x][0]->height - points[last_x][1]->height) * 2;
 
-        normals[points.size() - 1][points[0].size() - 1][0] = (points[points.size() - 1][points[0].size() - 1]->position[1] - points[points.size() - 2][points[0].size() - 1]->position[1]) * 2;
-        normals[points.size() - 1][points[0].size() - 1][1] = 2;
-        normals[points.size() - 1][points[0].size() - 1][2] = (points[points.size() - 1][points[0].size() - 2]->position[1] - points[points.size() - 1][points[0].size() - 1]->position[1]) * 2;
+        normals[last_x][last_y][0] = (points[last_x - 1][last_y]->height - points[last_x][last_y]->height) * 2;
+        normals[last_x][last_y][1] = (points[last_x][last_y - 1]->height - points[last_x][last_y]->height) * 2;
 
-        
+        double center_x = (points.size() - 1) / 2.0;
+        double center_z = (points[0].size() - 1) / 2.0;
+
+        material.apply();
+
         for (size_t i = 0; i < points.size() - 1; i++) {
             glBegin(GL_TRIANGLE_STRIP);
             for (size_t j = 0; j < points[0].size(); j++) {
-                glNormal3d(normals[i][j][0], normals[i][j][1], normals[i][j][2]);
-				glVertex3d(points[i][j]->position[0], points[i][j]->position[1], points[i][j]->position[2]);
+                glNormal3d(normals[i][j][0], NORMAL_Y, normals[i][j][1]);
+				glVertex3d((i - center_x) * 0.1, points[i][j]->height, (j - center_z) * 0.1);
 
-				glNormal3d(normals[i + 1][j][0], normals[i + 1][j][1], normals[i + 1][j][2]);
-				glVertex3d(points[i + 1][j]->position[0], points[i + 1][j]->position[1], points[i + 1][j]->position[2]);
+				glNormal3d(normals[i + 1][j][0], NORMAL_Y, normals[i + 1][j][1]);
+				glVertex3d((i + 1 - center_x) * 0.1, points[i + 1][j]->height, (j - center_z) * 0.1);
             }
             glEnd();
 		}
     }
 };
 
-WaterSurface* waterSurface;
-struct Params {
-    float p[4];
-    Params() = default;
-    Params(float* p) {
-        for (int i = 0; i < 4; i++)
-        {
-            this->p[i] = p[i];
-        }
-    }
-
-    Params& alpha(float a) {
-		p[3] = a;
-		return *this;
-	}
-    operator float*() { return p; }
-};
-
-Params multiply(Params a, double value)
-{
-	Params result;
-    result.p[3] = a.p[3];
-    for (int i = 0; i < 3; i++)
-    {
-		result.p[i] = a.p[i] * value;
-	}
-	return result;
-
-}
 void display();
 void update(int timer_id);
 void drawWaterSurface(int timer_id);
 void rain(int timer_id);
+
+WaterSurface* waterSurface;
 
 int main(int argc, char** argv)
 {
     glutInit(&argc, argv);
 
     waterSurface = new WaterSurface(100, 100, 0.1);
-    
-    waterSurface->setHeight(50, 50, 2.0);
-
+    waterSurface->material = Material(black, Params(white).alpha(0.1), Params(white).maltiply(0.000000000000000000001f).alpha(0.5), 128);
 
     glutInitWindowPosition(0, 0);
-    glutInitWindowSize(400, 400);
+    glutInitWindowSize(800, 800);
     glutInitDisplayMode(GLUT_RGBA | GLUT_DEPTH | GLUT_ALPHA);
     glutCreateWindow("“d20-0070 ¬–x³Ž÷");
     glClearColor(0.0, 0.0, 0.0, 1.0);
@@ -228,7 +235,6 @@ int main(int argc, char** argv)
     glEnable(GL_DEPTH_TEST);
 
     float light_position[] = { 1.0, 1.0, -1.0, 0.0};
-    float white_and_transparent[] = { 1.0, 1.0, 1.0, 0.5 };
     glLightfv(GL_LIGHT0, GL_POSITION, light_position);
     glLightfv(GL_LIGHT0, GL_DIFFUSE, white);
     glLightfv(GL_LIGHT0, GL_SPECULAR, white);
@@ -249,8 +255,10 @@ void display()
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
 
+    glTranslated(0, 0, -5);
+
     // ”wŒi
-    glMaterialfv(GL_FRONT, GL_DIFFUSE, multiply(white, 0.5));
+    glMaterialfv(GL_FRONT, GL_DIFFUSE, Params(white).maltiply(0.5));
     glMaterialfv(GL_FRONT, GL_SPECULAR, black);
     glMaterialfv(GL_FRONT, GL_AMBIENT, black);
     glMaterialf(GL_FRONT, GL_SHININESS, 0);
@@ -262,15 +270,14 @@ void display()
     glVertex3d(10, -1, -10);
     glEnd();
 
-    glTranslated(-5, 0, -10);
+    glTranslated(0, 0, -5);
 
     if (waterSurface != nullptr)
     {
-        glMaterialfv(GL_FRONT, GL_DIFFUSE, multiply(white_and_transparent, 1).alpha(0.1));
-        glMaterialfv(GL_FRONT, GL_SPECULAR, multiply(white_and_transparent, 0.000000000000000000001f));
-        //glMaterialfv(GL_FRONT, GL_SPECULAR, multiply(white_and_transparent, 0.00001f));
-        glMaterialf(GL_FRONT, GL_SHININESS, 128);
+        glPushMatrix();
+        glScaled(1, 1, 1);
         waterSurface->draw();
+        glPopMatrix();
     }
 
     glFlush();
